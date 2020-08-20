0527735c7d83b501854324549e8aa0299cbcbb6f
Pedro Borges <oi@pedroborg.es>
2020-08-20 11:01:50 -0300

    Add TextareaInput

---
+++ resources/js/Shared/TextareaInput.svelte
@@ -0,0 +1,50 @@
+<script>
+  import { onMount } from 'svelte'
+  import fitTextarea from 'fit-textarea'
+  import nanoid from 'nanoid'
+  import Label from '@/Components/Form/Label.svelte'
+
+  export let id = `textarea-input-${nanoid(5)}`
+  export let label
+  export let value
+  export let autosize = false
+  export let errors = []
+
+  let input
+
+  export const focus = () => input.focus()
+  export const select = () => input.select()
+
+  $: props = {
+    ...$$restProps,
+    class: 'form-textarea',
+  }
+
+  $: error = errors !== undefined && errors.length > 0
+
+  onMount(() => {
+    if (autosize) {
+      fitTextarea.watch(input)
+    }
+  })
+
+  function update(event) {
+    value = event.target.value
+  }
+</script>
+
+<div class={$$restProps.class}>
+  <Label {label} {id} />
+
+  <textarea
+    {...props}
+    bind:this={input}
+    class:error
+    {id}
+    {value}
+    on:input={update} />
+
+  {#if error}
+    <div class="form-error">{errors[0]}</div>
+  {/if}
+</div>
