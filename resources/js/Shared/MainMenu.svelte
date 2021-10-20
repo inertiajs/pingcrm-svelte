8b5eee1e541a0fe7d33c2974f97bfb28f504107d
Pedro Borges <oi@pedroborg.es>
2021-10-19 21:09:43 -0300

    Fix MainMenu current URL check

--- resources/js/Shared/MainMenu.svelte
+++ resources/js/Shared/MainMenu.svelte
@@ -4,7 +4,13 @@
   import Icon from '@/Shared/Icon.svelte'
 
   $: isUrl = (...urls) => {
-    return urls.filter(url => $page.url.match(url)).length
+    let currentUrl = $page.url.substr(1)
+
+    if (urls[0] === '') {
+      return currentUrl === ''
+    }
+
+    return urls.filter(url => currentUrl.startsWith(url)).length
   }
 </script>
 
@@ -13,8 +19,8 @@
     <a use:inertia href={route('dashboard')} class="flex items-center group py-3">
       <Icon
         name="dashboard"
-        class="w-4 h-4 mr-2 {isUrl('/$') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'}" />
-      <div class={isUrl('/$') ? 'text-white' : 'text-indigo-300 group-hover:text-white'}>
+        class="w-4 h-4 mr-2 {isUrl('') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'}" />
+      <div class={isUrl('') ? 'text-white' : 'text-indigo-300 group-hover:text-white'}>
         Dashboard
       </div>
     </a>
@@ -23,8 +29,8 @@
     <a use:inertia href={route('organizations')} class="flex items-center group py-3">
       <Icon
         name="office"
-        class="w-4 h-4 mr-2 {isUrl('/organizations/*') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'}" />
-      <div class={isUrl('/organizations/*') ? 'text-white' : 'text-indigo-300 group-hover:text-white'}>
+        class="w-4 h-4 mr-2 {isUrl('organizations') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'}" />
+      <div class={isUrl('organizations') ? 'text-white' : 'text-indigo-300 group-hover:text-white'}>
         Organizations
       </div>
     </a>
@@ -33,8 +39,8 @@
     <a use:inertia href={route('contacts')} class="flex items-center group py-3">
       <Icon
         name="users"
-        class="w-4 h-4 mr-2 {isUrl('/contacts/*') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'}" />
-      <div class={isUrl('/contacts/*') ? 'text-white' : 'text-indigo-300 group-hover:text-white'}>
+        class="w-4 h-4 mr-2 {isUrl('contacts') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'}" />
+      <div class={isUrl('contacts') ? 'text-white' : 'text-indigo-300 group-hover:text-white'}>
         Contacts
       </div>
     </a>
@@ -43,8 +49,8 @@
     <a use:inertia href={route('reports')} class="flex items-center group py-3">
       <Icon
         name="printer"
-        class="w-4 h-4 mr-2 {isUrl('/reports/*') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'}" />
-      <div class={isUrl('/reports/*') ? 'text-white' : 'text-indigo-300 group-hover:text-white'}>
+        class="w-4 h-4 mr-2 {isUrl('reports') ? 'fill-white' : 'fill-indigo-400 group-hover:fill-white'}" />
+      <div class={isUrl('reports') ? 'text-white' : 'text-indigo-300 group-hover:text-white'}>
         Reports
       </div>
     </a>
