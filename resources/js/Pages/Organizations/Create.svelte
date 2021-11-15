c827cdda15480af14dc05b664a24d09e22923078
Pedro Borges <oi@pedroborg.es>
2021-11-15 09:39:33 -0300

    Update create organization form to use the new Inertia form

--- resources/js/Pages/Organizations/Create.svelte
+++ resources/js/Pages/Organizations/Create.svelte
@@ -4,19 +4,15 @@
 </script>
 
 <script>
-  import { Inertia } from '@inertiajs/inertia'
-  import { inertia, remember } from '@inertiajs/inertia-svelte'
+  import { inertia, useForm } from '@inertiajs/inertia-svelte'
   import { route } from '@/Utils'
   import LoadingButton from '@/Shared/LoadingButton.svelte'
   import SelectInput from '@/Shared/SelectInput.svelte'
   import TextInput from '@/Shared/TextInput.svelte'
 
-  export let errors = {}
-
   $title = 'Create Organization'
 
-  let sending = false
-  let form = remember({
+  let form = useForm({
     name: null,
     email: null,
     phone: null,
@@ -28,10 +24,7 @@
   })
 
   function submit() {
-    Inertia.post(route('organizations.store'), $form, {
-      onStart: () => sending = true,
-      onFinish: () => sending = false,
-    })
+    $form.post(route('organizations.store'))
   }
 </script>
 
@@ -47,37 +40,37 @@
     <div class="p-8 -mr-6 -mb-8 flex flex-wrap">
       <TextInput
         bind:value={$form.name}
-        error={errors.name}
+        error={$form.errors.name}
         class="pr-6 pb-8 w-full lg:w-1/2"
         label="Name:" />
       <TextInput
         bind:value={$form.email}
-        error={errors.email}
+        error={$form.errors.email}
         class="pr-6 pb-8 w-full lg:w-1/2"
         label="Email:" />
       <TextInput
         bind:value={$form.phone}
-        error={errors.phone}
+        error={$form.errors.phone}
         class="pr-6 pb-8 w-full lg:w-1/2"
         label="Phone:" />
       <TextInput
         bind:value={$form.address}
-        error={errors.address}
+        error={$form.errors.address}
         class="pr-6 pb-8 w-full lg:w-1/2"
         label="Address:" />
       <TextInput
         bind:value={$form.city}
-        error={errors.city}
+        error={$form.errors.city}
         class="pr-6 pb-8 w-full lg:w-1/2"
         label="City:" />
       <TextInput
         bind:value={$form.region}
-        error={errors.region}
+        error={$form.errors.region}
         class="pr-6 pb-8 w-full lg:w-1/2"
         label="Province/State:" />
       <SelectInput
         bind:value={$form.country}
-        error={errors.country}
+        error={$form.errors.country}
         class="pr-6 pb-8 w-full lg:w-1/2"
         label="Country:">
         <option value={null} />
@@ -86,12 +79,12 @@
       </SelectInput>
       <TextInput
         bind:value={$form.postal_code}
-        error={errors.postal_code}
+        error={$form.errors.postal_code}
         class="pr-6 pb-8 w-full lg:w-1/2"
         label="Postal code:" />
     </div>
     <div class="px-8 py-4 bg-gray-100 border-t border-gray-200 flex justify-end items-center">
-      <LoadingButton loading={sending} class="btn-indigo" type="submit">
+      <LoadingButton loading={$form.processing} class="btn-indigo" type="submit">
         Create Organization
       </LoadingButton>
     </div>
