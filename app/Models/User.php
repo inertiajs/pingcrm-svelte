bfa7d59cb4903026c06e818b73e9d8da0e51ab6d
Pedro Borges <oi@pedroborg.es>
2021-11-15 10:27:35 -0300

    Update app to match latest Laravel defaults and best practices

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Foundation\Auth\Access\Authorizable;
-use Illuminate\Support\Facades\App;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\URL;
-use League\Glide\Server;
 
 class User extends Authenticatable
 {
@@ -21,6 +18,11 @@ class User extends Authenticatable
         'owner' => 'boolean',
         'email_verified_at' => 'datetime',
     ];
+
+    public function resolveRouteBinding($value, $field = null)
+    {
+        return $this->where($field ?? 'id', $value)->withTrashed()->firstOrFail();
+    }
 
     public function account()
     {
@@ -35,13 +37,6 @@ class User extends Authenticatable
     public function setPasswordAttribute($password)
     {
         $this->attributes['password'] = Hash::needsRehash($password) ? Hash::make($password) : $password;
-    }
-
-    public function photoUrl(array $attributes)
-    {
-        if ($this->photo_path) {
-            return URL::to(App::make(Server::class)->fromPath($this->photo_path, $attributes));
-        }
     }
 
     public function isDemoUser()
