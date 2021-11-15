a3fe7f59add45be6b2a0410d4f43a14c85be571a
Pedro Borges <oi@pedroborg.es>
2021-11-15 09:33:17 -0300

    Update create contact form to use the new Inertia form

--- resources/js/Pages/Contacts/Create.svelte
+++ resources/js/Pages/Contacts/Create.svelte
@@ -5,19 +5,17 @@
 
 <script>
   import { Inertia } from '@inertiajs/inertia'
-  import { inertia, remember } from '@inertiajs/inertia-svelte'
+  import { inertia, useForm } from '@inertiajs/inertia-svelte'
   import { route } from '@/Utils'
   import LoadingButton from '@/Shared/LoadingButton.svelte'
   import SelectInput from '@/Shared/SelectInput.svelte'
   import TextInput from '@/Shared/TextInput.svelte'
 
-  export let errors = {}
   export let organizations = []
 
   $title = 'Create Contact'
 
-  let sending = false
-  let form = remember({
+  let form = useForm({
     first_name: null,
     last_name: null,
     organization_id: null,
@@ -31,10 +29,7 @@
   })
 
   function submit() {
-    Inertia.post(route('contacts.store'), $form, {
-      onStart: () => sending = true,
-      onFinish: () => sending = false,
-    })
+    $form.post(route('contacts.store'))
   }
 </script>
 
@@ -49,17 +44,17 @@
     <div class="p-8 -mr-6 -mb-8 flex flex-wrap">
       <TextInput
         bind:value={$form.first_name}
-        error={errors.first_name}
+        error={$form.errors.first_name}
         class="pr-6 pb-8 w-full lg:w-1/2"
         label="First name:" />
       <TextInput
         bind:value={$form.last_name}
-        error={errors.last_name}
+        error={$form.errors.last_name}
         class="pr-6 pb-8 w-full lg:w-1/2"
         label="Last name:" />
       <SelectInput
         bind:value={$form.organization_id}
-        error={errors.organization_id}
+        error={$form.errors.organization_id}
         class="pr-6 pb-8 w-full lg:w-1/2"
         label="Organization:">
         <option value={null} />
@@ -69,32 +64,32 @@
       </SelectInput>
       <TextInput
         bind:value={$form.email}
-        error={errors.email}
+        error={$form.errors.email}
         class="pr-6 pb-8 w-full lg:w-1/2"
         label="Email:" />
       <TextInput
         bind:value={$form.phone}
-        error={errors.phone}
+        error={$form.errors.phone}
         class="pr-6 pb-8 w-full lg:w-1/2"
         label="Phone:" />
       <TextInput
         bind:value={$form.address}
-        error={errors.address}
+        error={$form.errors.address}
         class="pr-6 pb-8 w-full lg:w-1/2"
         label="Address:" />
       <TextInput
         bind:value={$form.city}
-        error={errors.city}
+        error={$form.errors.city}
         class="pr-6 pb-8 w-full lg:w-1/2"
         label="City:" />
       <TextInput
         bind:value={$form.region}
-        error={errors.region}
+        error={$form.errors.region}
         class="pr-6 pb-8 w-full lg:w-1/2"
         label="Province/State:" />
       <SelectInput
         bind:value={$form.country}
-        error={errors.country}
+        error={$form.errors.country}
         class="pr-6 pb-8 w-full lg:w-1/2"
         label="Country:">
         <option value={null} />
@@ -103,12 +98,12 @@
       </SelectInput>
       <TextInput
         bind:value={$form.postal_code}
-        error={errors.postal_code}
+        error={$form.errors.postal_code}
         class="pr-6 pb-8 w-full lg:w-1/2"
         label="Postal code:" />
     </div>
     <div class="px-8 py-4 bg-gray-100 border-t border-gray-200 flex justify-end items-center">
-      <LoadingButton loading={sending} class="btn-indigo" type="submit">
+      <LoadingButton loading={$form.processing} class="btn-indigo" type="submit">
         Create Contact
       </LoadingButton>
     </div>
